842017f1c1f94a477227590e3e55d88b3cda384f
Marco Prado <marco.asprado91@gmail.com>
2020-06-03 19:58:23 -0300

    Configure oauth user creation

--- gateway/src/main/java/br/com/marco/cadeacerva/gateway/infra/client/UsersClient.java
+++ gateway/src/main/java/br/com/marco/cadeacerva/gateway/infra/client/UsersClient.java
@@ -2,13 +2,11 @@ package br.com.marco.cadeacerva.gateway.infra.client;
 
 import br.com.marco.cadeacerva.gateway.infra.client.dto.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient("users")
 public interface UsersClient {
 
-    @PutMapping("/user/{email}")
+    @RequestMapping(method = RequestMethod.PUT, value = "/user/{email}")
     UserDTO saveUser(@PathVariable("email") String email, UserDTO user);
 }
--- gateway/src/main/java/br/com/marco/cadeacerva/gateway/infra/client/dto/UserDTO.java
+++ gateway/src/main/java/br/com/marco/cadeacerva/gateway/infra/client/dto/UserDTO.java
@@ -1,11 +1,14 @@
 package br.com.marco.cadeacerva.gateway.infra.client.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 
 @Getter
 @RequiredArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserDTO {
 
     public String id;
