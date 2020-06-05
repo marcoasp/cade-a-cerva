e4928b358382b5b20af0a956ca6180f18ab442b5
Marco Prado <marco.asprado91@gmail.com>
2020-06-04 23:41:31 -0300

    Add config server

---
+++ config/src/test/java/br/com/marco/cadeacerva/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marco.cadeacerva.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- gateway/src/main/java/br/com/marco/cadeacerva/gateway/GatewayApplication.java
+++ gateway/src/main/java/br/com/marco/cadeacerva/gateway/GatewayApplication.java
@@ -3,7 +3,6 @@ package br.com.marco.cadeacerva.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
